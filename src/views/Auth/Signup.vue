16d5eaebd80048ff7b361336498c3645c4dee233
Luma <lumatavares501@gmail.com>
2025-10-02 09:27:28 -0300

    login front

--- src/views/Auth/Signup.vue
+++ src/views/Auth/Signup.vue
@@ -228,7 +228,7 @@
                           <div
                             :class="
                               agreeToTerms
-                                ? 'border-brand-500 bg-brand-500'
+                                ? 'border-brand-950 bg-brand-950'
                                 : 'bg-transparent border-gray-300 dark:border-gray-700'
                             "
                             class="mr-3 flex h-5 w-5 items-center justify-center rounded-md border-[1.25px]"
@@ -267,7 +267,7 @@
                   <div>
                     <button
                       type="submit"
-                      class="flex items-center justify-center w-full px-4 py-3 text-sm font-medium text-white transition rounded-lg bg-brand-500 shadow-theme-xs hover:bg-brand-600"
+                      class="flex items-center justify-center w-full px-4 py-3 text-sm font-medium text-white transition rounded-lg bg-brand-950 shadow-theme-xs hover:bg-brand-950"
                     >
                       Sign Up
                     </button>
@@ -276,34 +276,19 @@
               </form>
               <div class="mt-5">
                 <p
-                  class="text-sm font-normal text-center text-gray-700 dark:text-gray-400 sm:text-start"
+                  class="text-sm font-normal text-center text-gray-950 dark:text-gray-950 sm:text-start"
                 >
                   Already have an account?
                   <router-link
                     to="/signin"
-                    class="text-brand-500 hover:text-brand-600 dark:text-brand-400"
+                    class="text-brand-950 hover:text-brand-950 dark:text-brand-400"
                     >Sign In</router-link
                   >
                 </p>
               </div>
             </div>
           </div>
-        </div>
-        <div
-          class="relative items-center hidden w-full h-full lg:w-1/2 bg-brand-950 dark:bg-white/5 lg:grid"
-        >
-          <div class="flex items-center justify-center z-1">
-            <common-grid-shape />
-            <div class="flex flex-col items-center max-w-xs">
-              <router-link to="/" class="block mb-4">
-                <img width="{231}" height="{48}" src="/images/logo/auth-logo.svg" alt="Logo" />
-              </router-link>
-              <p class="text-center text-gray-400 dark:text-white/60">
-                Free and Open-Source Tailwind CSS Admin Dashboard Template
-              </p>
-            </div>
-          </div>
-        </div>
+        </div>  
       </div>
     </div>
   </FullScreenLayout>
